342e6934575e183af3629576eacc71dea8fd502a
Nikita Shpulin <shpulinn@mail.ru>
2025-05-02 17:22:05 +0200

    new input controller script for buttons interaction

---
+++ assets/Scripts/InputController.ts
@@ -0,0 +1,42 @@
+import { _decorator, Component, Button, Input } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('InputController')
+export class InputController extends Component {
+    @property(Button)
+    private leftButton: Button = null;
+
+    @property(Button)
+    private rightButton: Button = null;
+
+    private moveDirection: number = 0;
+
+    onLoad() {
+        this.leftButton.node.on(Input.EventType.TOUCH_START, this.startMoveLeft, this);
+        this.leftButton.node.on(Input.EventType.TOUCH_END, this.stopMove, this);
+        this.rightButton.node.on(Input.EventType.TOUCH_START, this.startMoveRight, this);
+        this.rightButton.node.on(Input.EventType.TOUCH_END, this.stopMove, this);
+    }
+
+    startMoveLeft() {
+        this.moveDirection = -1;
+        this.node.emit('moveInput', this.moveDirection);
+    }
+
+    startMoveRight() {
+        this.moveDirection = 1;
+        this.node.emit('moveInput', this.moveDirection);
+    }
+
+    stopMove() {
+        this.moveDirection = 0;
+        this.node.emit('moveInput', this.moveDirection);
+    }
+
+    onDestroy() {
+        this.leftButton.node.off(Input.EventType.TOUCH_START, this.startMoveLeft, this);
+        this.leftButton.node.off(Input.EventType.TOUCH_END, this.stopMove, this);
+        this.rightButton.node.off(Input.EventType.TOUCH_START, this.startMoveRight, this);
+        this.rightButton.node.off(Input.EventType.TOUCH_END, this.stopMove, this);
+    }
+}
